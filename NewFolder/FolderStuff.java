4dd2185ebf461b8d57e6ca27a69d0bf15d9cc917
Caelan Quach <caelanquach@gmail.com>
2024-10-13 21:13:47 -0500

    Adding a Folder to the Repository

---
+++ NewFolder/FolderStuff.java
@@ -0,0 +1,7 @@
+package NewFolder;
+
+public class FolderStuff {
+    public static void main(String[] args){
+        System.out.println("Nothing to see in here but the folder");
+    }
+}
--- TestingScript.java
+++ TestingScript.java
@@ -3,7 +3,6 @@ public class TestingScript {
     public static void main(String[] args){
 
         System.out.println("Hello World");
-        System.out.println("Stinky");
 
     }
 
